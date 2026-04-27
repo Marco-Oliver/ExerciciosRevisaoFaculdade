f843d197464f8e9f338359367b352d82221b480c
Marco-Oliver <marcooliver2@yahoo.com>
2026-04-27 15:09:34 -0300

    Exercicio4

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function alterarTexto() {
-    let novoTexto = prompt("Digite um novo texto:");
+function calcular() {
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let op = document.getElementById("operacao").value;
+    let resultado;
 
-    if (novoTexto) {
-        document.getElementById("paragrafo").innerText = novoTexto;
+    if (op === "soma") {
+        resultado = n1 + n2;
+    } else if (op === "subtracao") {
+        resultado = n1 - n2;
+    } else if (op === "multiplicacao") {
+        resultado = n1 * n2;
+    } else if (op === "divisao") {
+        resultado = n2 !== 0 ? n1 / n2 : "Erro: divisão por zero";
     }
+
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
 }
