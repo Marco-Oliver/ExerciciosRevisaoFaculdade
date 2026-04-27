37a10bddd0ba1c2b9395a858d07cd62d9acd6062
Marco-Oliver <marcooliver2@yahoo.com>
2026-04-27 13:19:52 -0300

    Exercicio2

---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = "Marco";
+var idade = 18;
+var altura = 1.75;
+var estudante = true;
+
+alert(
+  "Nome: " + nome + " (tipo: " + typeof nome + ") | " +
+  "Idade: " + idade + " (tipo: " + typeof idade + ") | " +
+  "Altura: " + altura + " (tipo: " + typeof altura + ") | " +
+  "Estudante: " + estudante + " (tipo: " + typeof estudante + ")"
+);
